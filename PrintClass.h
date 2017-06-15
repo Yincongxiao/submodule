3710713a049c539f01f32989addb916f9d0cca06
Congxiao Yin <asnailneo@gmail.com>
2017-06-15 10:48:46 +0800

    add printClass

---
+++ PrintClass.h
@@ -0,0 +1,15 @@
+//
+//  PrintClass.h
+//  SubmoduleTest
+//
+//  Created by CongXiao Yin on 2017/6/15.
+//  Copyright © 2017年 Personal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrintClass : NSObject
+
+- (void)printSomething:(NSString *)string;
+
+@end
---
+++ PrintClass.m
@@ -0,0 +1,17 @@
+//
+//  PrintClass.m
+//  SubmoduleTest
+//
+//  Created by CongXiao Yin on 2017/6/15.
+//  Copyright © 2017年 Personal. All rights reserved.
+//
+
+#import "PrintClass.h"
+
+@implementation PrintClass
+
+- (void)printSomething:(NSString *)string {
+    NSLog(@"%@",string);
+}
+
+@end
